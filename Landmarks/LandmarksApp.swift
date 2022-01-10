fd3d46abf55e6c1f7175c22b8458438c9d82c0c4
Florian Bruder <florian.bruder@posteo.de>
2022-01-10 20:10:54 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Florian Bruder on 10.01.22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
