69f8e80645e536c84518aa0e21442642e30f23b5
Florian Bruder <florian.bruder@posteo.de>
2022-01-10 21:09:45 +0100

    Add MapView.swift

---
+++ Landmarks/UI/MapView.swift
@@ -0,0 +1,24 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Florian Bruder on 10.01.22.
+//
+
+import SwiftUI
+
+// MARK: -
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: -
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
