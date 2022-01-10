1902b897c88aa3ebe0437d8dd8c5b0389b4d3a5a
Florian Bruder <florian.bruder@posteo.de>
2022-01-10 21:20:00 +0100

    Complete MapView

--- Landmarks/UI/MapView.swift
+++ Landmarks/UI/MapView.swift
@@ -5,13 +5,19 @@
 //  Created by Florian Bruder on 10.01.22.
 //
 
+import MapKit
 import SwiftUI
 
 // MARK: -
 
 struct MapView: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 34.011286, longitude: -116.166868),
+        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+    )
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Map(coordinateRegion: $region)
     }
 }
 
