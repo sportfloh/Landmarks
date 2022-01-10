18091a48eee0c1c6d3723b86caff5011cae74ba6
Florian Bruder <florian.bruder@posteo.de>
2022-01-10 20:57:58 +0100

    Add CircleImageView.swift

---
+++ Landmarks/UI/CircleImageView.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImageView.swift
+//  Landmarks
+//
+//  Created by Florian Bruder on 10.01.22.
+//
+
+import SwiftUI
+
+// MARK: -
+
+struct CircleImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: -
+
+struct CircleImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImageView()
+    }
+}
