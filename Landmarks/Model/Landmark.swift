7867662dcb52ba1a647393e282754cef3decf073
Florian Bruder <florian.bruder@posteo.de>
2022-01-11 21:25:31 +0100

    Add struct Landmark

--- Landmarks/Model/Landmark.swift
+++ Landmarks/Model/Landmark.swift
@@ -8,3 +8,11 @@
 import Foundation
 
 // MARK: -
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+}
