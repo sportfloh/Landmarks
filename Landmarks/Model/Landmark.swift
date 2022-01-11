f9a1e6fb993d266cd811e66ea66fd293fb050efd
Florian Bruder <florian.bruder@posteo.de>
2022-01-11 21:46:56 +0100

    Complete struct Landmark

--- Landmarks/Model/Landmark.swift
+++ Landmarks/Model/Landmark.swift
@@ -5,7 +5,9 @@
 //  Created by Florian Bruder on 11.01.22.
 //
 
+import CoreLocation
 import Foundation
+import SwiftUI
 
 // MARK: -
 
@@ -15,4 +17,20 @@ struct Landmark: Hashable, Codable {
     var park: String
     var state: String
     var description: String
+
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude, longitude: coordinates.longitude)
+    }
 }
