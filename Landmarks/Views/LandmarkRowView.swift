3faac7a64413a7d3f71b05249b6f0598bae9631f
Florian Bruder <florian.bruder@posteo.de>
2022-01-11 22:02:50 +0100

    Add LandmarkRowView.swift

---
+++ Landmarks/Views/LandmarkRowView.swift
@@ -0,0 +1,24 @@
+//
+//  LandmarkRowView.swift
+//  Landmarks
+//
+//  Created by Florian Bruder on 11.01.22.
+//
+
+import SwiftUI
+
+// MARK: -
+
+struct LandmarkRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: -
+
+struct LandmarkRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRowView()
+    }
+}
