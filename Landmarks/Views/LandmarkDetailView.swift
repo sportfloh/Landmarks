ebbab1a1aa269632512b62c741e4bd284b3e7572
Florian Bruder <florian.bruder@posteo.de>
2022-01-12 22:29:16 +0100

    Move detail view from ContentView.swift to LandmarkDetailView.swift

--- Landmarks/Views/LandmarkDetailView.swift
+++ Landmarks/Views/LandmarkDetailView.swift
@@ -11,7 +11,37 @@ import SwiftUI
 
 struct LandmarkDetailView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+
+            CircleImageView()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+
+                Divider()
+                Text("About Turtle Rock")
+                    .font(.title2)
+
+                Text("Descriptive text goes here.")
+            }
+            .padding()
+
+            Spacer()
+        }
     }
 }
 
