e7202658b949be6e7db617461be82e4338bfbed1
Florian Bruder <florian.bruder@posteo.de>
2022-01-12 22:26:21 +0100

    Add LandmarkDetailView.swift

---
+++ Landmarks/Views/LandmarkDetailView.swift
@@ -0,0 +1,24 @@
+//
+//  LandmarkDetailView.swift
+//  Landmarks
+//
+//  Created by Florian Bruder on 12.01.22.
+//
+
+import SwiftUI
+
+// MARK: -
+
+struct LandmarkDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: -
+
+struct LandmarkDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetailView()
+    }
+}
