89683eae43f2b93199f92a7ab741f50f9ef6a3f1
Florian Bruder <florian.bruder@posteo.de>
2022-01-17 12:42:32 +0100

    Add FavoriteButtonView.swift

---
+++ Landmarks/Views/FavoriteButtonView.swift
@@ -0,0 +1,24 @@
+//
+//  FavoriteButtonView.swift
+//  Landmarks
+//
+//  Created by Florian Bruder on 17.01.22.
+//
+
+import SwiftUI
+
+// MARK: -
+
+struct FavoriteButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: -
+
+struct FavoriteButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButtonView()
+    }
+}
