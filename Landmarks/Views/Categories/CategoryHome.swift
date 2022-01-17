5471b72818906cb9ef022be32cf83057d1bac97d
Florian Bruder <florian.bruder@posteo.de>
2022-01-17 20:06:19 +0100

    Add CategoryHome.swift

---
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Florian Bruder on 17.01.22.
+//
+
+import SwiftUI
+
+// MARK: -
+
+struct CategoryHome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: -
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
