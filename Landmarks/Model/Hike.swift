32b3ad06746aa8eaf860b86dd7e6da4acfa0eafd
Florian Bruder <florian.bruder@posteo.de>
2022-01-17 17:25:58 +0100

    Complete the Hike type

--- Landmarks/Model/Hike.swift
+++ Landmarks/Model/Hike.swift
@@ -5,6 +5,29 @@
 //  Created by Florian Bruder on 17.01.22.
 //
 
+import Combine
 import Foundation
 
 // MARK: -
+
+struct Hike: Codable, Hashable, Identifiable {
+    var id: Int
+    var name: String
+    var distance: Double
+    var difficulty: Int
+    var observations: [Observation]
+
+    static var formater = LengthFormatter()
+
+    var distanceTest: String {
+        Hike.formater.string(fromValue: distance, unit: .kilometer)
+    }
+
+    struct Observation: Codable, Hashable {
+        var distanceFromStart: Double
+
+        var elevation: Range<Double>
+        var pace: Range<Double>
+        var heartRate: Range<Double>
+    }
+}
