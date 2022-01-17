f7d6d23b68193e5bb9c8412d1f34239fd0711b09
Florian Bruder <florian.bruder@posteo.de>
2022-01-17 15:45:20 +0100

    Add Badge.swift

---
+++ Landmarks/Views/Badge.swift
@@ -0,0 +1,24 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by Florian Bruder on 17.01.22.
+//
+
+import SwiftUI
+
+// MARK: -
+
+struct Badge: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: -
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
