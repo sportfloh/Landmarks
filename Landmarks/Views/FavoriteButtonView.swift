2537b56214271ce86258aa990e23d270ba7ce94f
Florian Bruder <florian.bruder@posteo.de>
2022-01-17 12:48:15 +0100

    Add FavoriteButtonView

--- Landmarks/Views/FavoriteButtonView.swift
+++ Landmarks/Views/FavoriteButtonView.swift
@@ -10,8 +10,16 @@ import SwiftUI
 // MARK: -
 
 struct FavoriteButtonView: View {
+    @Binding var isSet: Bool
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundColor(isSet ? .yellow : .gray)
+        }
     }
 }
 
@@ -19,6 +27,6 @@ struct FavoriteButtonView: View {
 
 struct FavoriteButtonView_Previews: PreviewProvider {
     static var previews: some View {
-        FavoriteButtonView()
+        FavoriteButtonView(isSet: .constant(true))
     }
 }
