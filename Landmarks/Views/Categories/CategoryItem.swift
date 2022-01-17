fd6ed9416acad6a22bc8ed00fbab23ddf7be57fe
Florian Bruder <florian.bruder@posteo.de>
2022-01-17 21:09:26 +0100

    Add CategoryItem.swift

---
+++ Landmarks/Views/Categories/CategoryItem.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryItem.swift
+//  Landmarks
+//
+//  Created by Florian Bruder on 17.01.22.
+//
+
+import SwiftUI
+
+// MARK: -
+
+struct CategoryItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: -
+
+struct CategoryItem_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryItem()
+    }
+}
