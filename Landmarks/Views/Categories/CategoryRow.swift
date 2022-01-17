e24d46ef37094e9242d7430ad64989022b0edad2
Florian Bruder <florian.bruder@posteo.de>
2022-01-17 20:48:06 +0100

    Add CategoryRow.swift

---
+++ Landmarks/Views/Categories/CategoryRow.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryRow.swift
+//  Landmarks
+//
+//  Created by Florian Bruder on 17.01.22.
+//
+
+import SwiftUI
+
+// MARK: -
+
+struct CategoryRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: -
+
+struct CategoryRow_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryRow()
+    }
+}
