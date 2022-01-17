d0215c48dfaa7e1d03d2cd832e2eabf5f8232259
Florian Bruder <florian.bruder@posteo.de>
2022-01-17 20:37:38 +0100

    Add categories property to ModelData

--- Landmarks/Model/ModelData.swift
+++ Landmarks/Model/ModelData.swift
@@ -13,6 +13,13 @@ import Foundation
 final class ModelData: ObservableObject {
     @Published var landmarks: [Landmark] = load("landmarkData.json")
     var hikes: [Hike] = load("hikeData.json")
+
+    var categories: [String: [Landmark]] {
+        Dictionary(
+            grouping: landmarks,
+            by: { $0.category.rawValue }
+        )
+    }
 }
 
 // MARK: -
