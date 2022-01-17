2aa1cb35268bf503107af54a0b4052aae4ac08ed
Florian Bruder <florian.bruder@posteo.de>
2022-01-17 15:28:37 +0100

    Add BadgeSymbol.swift

---
+++ Landmarks/Views/BadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  BadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Florian Bruder on 17.01.22.
+//
+
+import SwiftUI
+
+// MARK: -
+
+struct BadgeSymbol: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: -
+
+struct BadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeSymbol()
+    }
+}
