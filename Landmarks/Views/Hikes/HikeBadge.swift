9003e5bd38e0d3f5e81b8273114bb7fdc8dc2d62
Florian Bruder <florian.bruder@posteo.de>
2022-01-18 21:22:34 +0100

    Add HikeBadge.swift

---
+++ Landmarks/Views/Hikes/HikeBadge.swift
@@ -0,0 +1,24 @@
+//
+//  HikeBadge.swift
+//  Landmarks
+//
+//  Created by Florian Bruder on 18.01.22.
+//
+
+import SwiftUI
+
+// MARK: -
+
+struct HikeBadge: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: -
+
+struct HikeBadge_Previews: PreviewProvider {
+    static var previews: some View {
+        HikeBadge()
+    }
+}
