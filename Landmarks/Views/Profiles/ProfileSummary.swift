392a7c9a1ba1b092372a2f58cfc255b2353903ad
Florian Bruder <florian.bruder@posteo.de>
2022-01-18 21:42:41 +0100

    Complete ProfileSummary

--- Landmarks/Views/Profiles/ProfileSummary.swift
+++ Landmarks/Views/Profiles/ProfileSummary.swift
@@ -10,6 +10,7 @@ import SwiftUI
 // MARK: -
 
 struct ProfileSummary: View {
+    @EnvironmentObject var modelData: ModelData
     var profile: Profile
 
     var body: some View {
@@ -22,6 +23,35 @@ struct ProfileSummary: View {
                 Text("Notifications: \(profile.prefersNotifications ? "On" : "Off")")
                 Text("Seasonal Photos: \(profile.seasonalPhoto.rawValue)")
                 Text("Goal Date: ") + Text(profile.goalDate, style: .date)
+
+                Divider()
+
+                VStack(alignment: .leading) {
+                    Text("Completed Badges")
+                        .font(.headline)
+
+                    ScrollView(.horizontal) {
+                        HStack {
+                            HikeBadge(name: "First Hike")
+                            HikeBadge(name: "Earth Day")
+                                .hueRotation(Angle(degrees: 90))
+
+                            HikeBadge(name: "Tenth Hike")
+                                .grayscale(0.5)
+                                .hueRotation(Angle(degrees: 45))
+                        }
+                        .padding(.bottom)
+                    }
+                }
+
+                Divider()
+
+                VStack(alignment: .leading) {
+                    Text("Recent Hikes")
+                        .font(.headline)
+
+                    HikeView(hike: modelData.hikes[0])
+                }
             }
         }
     }
@@ -32,5 +62,6 @@ struct ProfileSummary: View {
 struct ProfileSummary_Previews: PreviewProvider {
     static var previews: some View {
         ProfileSummary(profile: Profile.default)
+            .environmentObject(ModelData())
     }
 }
