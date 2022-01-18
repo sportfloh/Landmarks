56856c98d5028391fd4b4eb0560eeee8217dc71b
Florian Bruder <florian.bruder@posteo.de>
2022-01-18 21:16:25 +0100

    Add ProfileSummary.swift

---
+++ Landmarks/Views/Profiles/ProfileSummary.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileSummary.swift
+//  Landmarks
+//
+//  Created by Florian Bruder on 18.01.22.
+//
+
+import SwiftUI
+
+// MARK: -
+
+struct ProfileSummary: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: -
+
+struct ProfileSummary_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileSummary()
+    }
+}
