6532c82b126972d059f0c97a7c5b57f34711eec0
Florian Bruder <florian.bruder@posteo.de>
2022-01-21 21:44:49 +0100

    Add ProfileEditor.swift

---
+++ Landmarks/Views/Profiles/ProfileEditor.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileEditor.swift
+//  Landmarks
+//
+//  Created by Florian Bruder on 21.01.22.
+//
+
+import SwiftUI
+
+// MARK: -
+
+struct ProfileEditor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: -
+
+struct ProfileEditor_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileEditor()
+    }
+}
