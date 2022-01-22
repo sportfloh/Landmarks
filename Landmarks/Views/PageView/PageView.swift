46b43b157ef5b9a84fad2fb167c593d24cbaf2cb
Florian Bruder <florian.bruder@posteo.de>
2022-01-22 15:59:58 +0100

    Add PageView.swift

---
+++ Landmarks/Views/PageView/PageView.swift
@@ -0,0 +1,24 @@
+//
+//  PageView.swift
+//  Landmarks
+//
+//  Created by Florian Bruder on 22.01.22.
+//
+
+import SwiftUI
+
+// MARK: -
+
+struct PageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: -
+
+struct PageView_Previews: PreviewProvider {
+    static var previews: some View {
+        PageView()
+    }
+}
