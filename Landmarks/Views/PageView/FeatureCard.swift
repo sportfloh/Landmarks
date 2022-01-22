17097f66bb8bb5404733b0d7a73115014eeadf35
Florian Bruder <florian.bruder@posteo.de>
2022-01-22 15:42:13 +0100

    Add FeatureCard.swift

---
+++ Landmarks/Views/PageView/FeatureCard.swift
@@ -0,0 +1,24 @@
+//
+//  FeatureCard.swift
+//  Landmarks
+//
+//  Created by Florian Bruder on 22.01.22.
+//
+
+import SwiftUI
+
+// MARK: -
+
+struct FeatureCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: -
+
+struct FeatureCard_Previews: PreviewProvider {
+    static var previews: some View {
+        FeatureCard()
+    }
+}
