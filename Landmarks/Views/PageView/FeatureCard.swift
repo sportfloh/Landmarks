369f646531168674a868476884a1a31e455f10cc
Florian Bruder <florian.bruder@posteo.de>
2022-01-22 15:58:59 +0100

    Complete FeatureCard type

--- Landmarks/Views/PageView/FeatureCard.swift
+++ Landmarks/Views/PageView/FeatureCard.swift
@@ -10,8 +10,41 @@ import SwiftUI
 // MARK: -
 
 struct FeatureCard: View {
+    var landmark: Landmark
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        landmark.featureImage?
+            .resizable()
+            .aspectRatio(3 / 2, contentMode: .fit)
+            .overlay(
+                TextOverlay(landmark: landmark))
+    }
+}
+
+// MARK: -
+
+struct TextOverlay: View {
+    var landmark: Landmark
+
+    var gradiant: LinearGradient {
+        .linearGradient(
+            Gradient(colors: [.black.opacity(0.6), .black.opacity(0)]),
+            startPoint: .bottom,
+            endPoint: .center)
+    }
+
+    var body: some View {
+        ZStack(alignment: .bottomLeading) {
+            gradiant
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .font(.title)
+                    .bold()
+                Text(landmark.park)
+            }
+            .padding()
+        }
+        .foregroundColor(.white)
     }
 }
 
@@ -19,6 +52,6 @@ struct FeatureCard: View {
 
 struct FeatureCard_Previews: PreviewProvider {
     static var previews: some View {
-        FeatureCard()
+        FeatureCard(landmark: ModelData().features[0])
     }
 }
