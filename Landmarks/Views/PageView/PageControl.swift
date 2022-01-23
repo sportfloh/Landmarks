f54adf9ea05d02e05f08d3dca7adbf4004eca735
Florian Bruder <florian.bruder@posteo.de>
2022-01-23 18:06:31 +0100

    Add PageControl.swift

---
+++ Landmarks/Views/PageView/PageControl.swift
@@ -0,0 +1,24 @@
+//
+//  PageControl.swift
+//  Landmarks
+//
+//  Created by Florian Bruder on 23.01.22.
+//
+
+import SwiftUI
+
+// MARK: -
+
+struct PageControl: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: -
+
+struct PageControl_Previews: PreviewProvider {
+    static var previews: some View {
+        PageControl()
+    }
+}
