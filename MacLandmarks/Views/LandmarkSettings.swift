53a7d7e719382dbae1f6a97423acd42dcb6fcb4a
Florian Bruder <florian.bruder@posteo.de>
2022-01-24 20:25:10 +0100

    Add LandmarkSettings.swift

---
+++ MacLandmarks/Views/LandmarkSettings.swift
@@ -0,0 +1,24 @@
+//
+//  LandmarkSettings.swift
+//  MacLandmarks
+//
+//  Created by Florian Bruder on 24.01.22.
+//
+
+import SwiftUI
+
+// MARK: -
+
+struct LandmarkSettings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: -
+
+struct LandmarkSettings_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkSettings()
+    }
+}
