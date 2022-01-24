e68163f4a42eda2c69ed796bcfc0576d38bca89e
Florian Bruder <florian.bruder@posteo.de>
2022-01-24 11:18:09 +0100

    Add LandmarkDetail.swift to watch extension

---
+++ WatchLandmarks WatchKit Extension/LandmarkDetail.swift
@@ -0,0 +1,24 @@
+//
+//  LandmarkDetail.swift
+//  WatchLandmarks WatchKit Extension
+//
+//  Created by Florian Bruder on 24.01.22.
+//
+
+import SwiftUI
+
+// MARK: -
+
+struct LandmarkDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: -
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail()
+    }
+}
