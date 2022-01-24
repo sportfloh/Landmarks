c287d899780bfbbc9a875bbddab15c9197ae8754
Florian Bruder <florian.bruder@posteo.de>
2022-01-24 14:01:23 +0100

    Add macOS target

---
+++ MacLandmarks/MacLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacLandmarksApp.swift
+//  MacLandmarks
+//
+//  Created by Florian Bruder on 24.01.22.
+//
+
+import SwiftUI
+
+@main
+struct MacLandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
