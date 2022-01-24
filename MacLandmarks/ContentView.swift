5e36915acba71c72a6f2fa9d6b831f514faa5e14
Florian Bruder <florian.bruder@posteo.de>
2022-01-24 14:14:19 +0100

    Add LandmarkList to macOS ConventView

--- MacLandmarks/ContentView.swift
+++ MacLandmarks/ContentView.swift
@@ -7,15 +7,20 @@
 
 import SwiftUI
 
+// MARK: -
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        LandmarkList()
+            .frame(minWidth: 7000, minHeight: 300)
     }
 }
+
+// MARK: -
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(ModelData())
     }
 }
