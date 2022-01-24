b6a41c24c9027efab06601edda20a9dc65d8a063
Florian Bruder <florian.bruder@posteo.de>
2022-01-24 16:47:35 +0100

    Add LandmarkList.swift

---
+++ WatchLandmarks WatchKit Extension/Views/LandmarkList.swift
@@ -0,0 +1,24 @@
+//
+//  LandmarkList.swift
+//  WatchLandmarks WatchKit Extension
+//
+//  Created by Florian Bruder on 24.01.22.
+//
+
+import SwiftUI
+
+// MARK: -
+
+struct LandmarkList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: -
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
